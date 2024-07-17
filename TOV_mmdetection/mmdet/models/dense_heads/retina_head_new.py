511261374556e182f7837d8b003ce76dbb702839
Fallborn <617924767@qq.com>
2024-07-17 23:24:34 +0800

    fix

--- TOV_mmdetection/mmdet/models/dense_heads/retina_head_new.py
+++ TOV_mmdetection/mmdet/models/dense_heads/retina_head_new.py
@@ -254,10 +254,10 @@ class RetinaHeadPADNN(AnchorHead):
                     norm_cfg=self.norm_cfg))
         # self.cls_convs.insert(len(self.cls_convs)-1,self.mda_cls)
         # self.reg_convs.insert(len(self.reg_convs)-1,self.mda_reg)
-        self.cls_convs.append(self.mda_cls)
-        self.reg_convs.append(self.mda_reg)
-        # self.cls_convs.append(self.ca_cls)
-        # self.reg_convs.append(self.ca_reg)
+        # self.cls_convs.append(self.mda_cls)
+        # self.reg_convs.append(self.mda_reg)
+        self.cls_convs.append(self.ca_cls)
+        self.reg_convs.append(self.ca_reg)
         # self.cls_convs.append(self.cbam_cls)
         # self.reg_convs.append(self.cbam_reg)
 
@@ -289,13 +289,13 @@ class RetinaHeadPADNN(AnchorHead):
                 cls_feat = cls_conv(cls_feat)
 
             else:
-                cls_feat = cls_feat + 0.00001*cls_conv(cls_feat)
+                cls_feat = cls_feat + 0.001*cls_conv(cls_feat)
 
         for i, reg_conv in enumerate(self.reg_convs):
             if i != len(self.reg_convs)-1:
                 reg_feat = reg_conv(reg_feat)
             else:
-                reg_feat = reg_feat + 0.00001*cls_conv(reg_feat)
+                reg_feat = reg_feat + 0.001*cls_conv(reg_feat)
 
         cls_score = self.retina_cls(cls_feat)
         bbox_pred = self.retina_reg(reg_feat)
