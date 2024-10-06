963dd253c4e31c2563f1fd7b69d9eb7f1b7cf2cf
Fallborn <617924767@qq.com>
2024-10-06 23:56:38 +0800

    fix

--- TOV_mmdetection/tools/test_retinanet.py
+++ TOV_mmdetection/tools/test_retinanet.py
@@ -3,23 +3,34 @@ from mmdet.apis import init_detector, inference_detector, show_result_pyplot
 # mmcv是mmdet之类的通用库
 import mmcv
 
-config_file = 'configs2/COCO/detection/retinanet_r50_fpn_1x_coco_padnn.py'
+config_file = 'configs2/COCO/detection/retinanet_r50_fpn_1x_coco_pcb.py'
 # checkpoint 需要自己手动下载，Model Zoo里应该有
-checkpoint_file = 'work_dirs/retinanet_r50_fpn_1x_coco_padnn/epoch_16.pth'
+checkpoint_file = 'work_dirs/retinanet_r50_fpn_1x_coco_pcb/epoch_12.pth'
 
 model = init_detector(config_file, checkpoint_file, device='cuda:3')  # or device='cuda:0'
 
-#crazing
+# neu-det
+# crazing
 # inclusion
 # patches
 # pitted_surface
 # rolled-in_scale
 # scratches
-classes = ['crazing', 'inclusion', 'patches', 'pitted_surface', 'rolled-in_scale', 'scratches']
+# classes = ['crazing', 'inclusion', 'patches', 'pitted_surface', 'rolled-in_scale', 'scratches']
+
+# open
+# short
+# mousebite
+# spur
+# copper
+# Pin-hole
+
+
+classes = ['missing_hole','mouse_bite','open_circuit','short','spur','spurious_copper']
 for cls in classes:
     for i in range(10):
         try:
-            img = 'data/coco/images/train/{}_{}.jpg'.format(cls,i)
+            img = 'data/coco_pcb/images/train/01_{}_0{}.jpg'.format(cls,i)
 
 
             result = inference_detector(model,img)
